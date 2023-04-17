20b00a2aadd781d7dd4dc8134a07ccacaceb1ab0
Rojith Peiris <rojithonline@gmail.com>
2023-04-17 19:15:05 +0530

    s

--- loader.js
+++ loader.js
@@ -6,14 +6,15 @@ var keyword = "admin";
 // Check if the current URL contains the keyword
 if (currentUrl.indexOf(keyword) !== -1) {
   //load all css and javascript file of website theam
-  console.log("keyword not found in url");
-} else {
   var head = document.getElementsByTagName("HEAD")[0];
   var link = document.createElement('link');
   link.rel = 'stylesheet';
   link.type = 'text/css';
-  link.href = 'css-loader.css';
+  link.href = 'https://cdn.jsdelivr.net/gh/rojapeiris/assets@main/css-loader.css';
   head.appendChild(link);
+  console.log("keyword not found in url");
+} else {
+  
   //load all css and javascript file of admin theam
   console.log("keyword found in url");
 }
